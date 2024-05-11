2c009616275570e90568221a6d46a4c1ba6b1fc8
chebupelka8 <stpzamyatin@gmail.com>
2024-05-11 13:23:17 +0500

    Configure some files

--- qtharmony/widgets/__init__.py
+++ qtharmony/widgets/__init__.py
@@ -1,10 +1,13 @@
 from .basic.button import PushButton
 from .basic.widgets_list import WidgetsList
 
-from .basic.switchers import (
-    Entry, DropDownMenu, DigitalEntry, 
-    CheckBox, PathEntry, RadioButton
-)
+from .basic.check_box import CheckBox
+from .basic.digital_entry import DigitalEntry
+from .basic.drop_down_menu import DropDownMenu
+from .basic.entry import Entry
+from .basic.radio_button import RadioButton
+from .basic.path_entry import PathEntry
+
 from .basic.label import Label
 from .basic.splitter import Splitter
 
--- qtharmony/widgets/basic/switchers.py
+++
@@ -1,6 +0,0 @@
-from .check_box import CheckBox
-from .digital_entry import DigitalEntry
-from .drop_down_menu import DropDownMenu
-from .entry import Entry
-from .radio_button import RadioButton
-from .path_entry import PathEntry
